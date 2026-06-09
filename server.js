2a14dbc5df05423c659dbe507f8b05911b1526d5
NidaRiaz <nidariaz316@gmail.com>
2026-06-09 19:40:11 +0500

    Fixed secrets and vulnerabilities

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
-const API_KEY = "sk-test-123456789"; 
+const API_KEY = process.env.API_KEY;
 const app = express();
 app.get("/", (req, res) => {
   res.send("DevSecOps Security Lab");
 });
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server Running");
+});
